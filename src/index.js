52df838d74aec73df636dbbc0e27f7d09d9f5c06
rajeev356jnu <rajeev356jnu@gmail.com>
2022-05-04 13:38:29 +0530

    n

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express =require('express');
+const router=require('./routes/route')
+const bodyParser= require('body-parser');
+const mongoose= require('mongoose');
+const app=express()
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded(({ extended: true })))
+
+mongoose.connect("mongodb+srv://functionup-uranium1:QzN5Crh5t5EcxZ6T@cluster0.149kg.mongodb.net/project2" ,
+ { useNewurlParser:true
+ })
+ .then (() => console.log("MongoDb is connected"))
+ .catch(err => console.log(err))
+
+
+app.use('/',router)
+
+
+
+
+app.listen(process.env.PORT||3000,function(){
+    console.log("port is running at:"+(process.env.PORT||3000))
+});
